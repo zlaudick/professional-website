6604efd3f0dc8a6ad846be44590a75137f81f666
Zac Laudick <zlaudick@nmsu.edu>
2016-07-20 13:35:02 -0600

    added Purpose, Audience, and Goal to milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,5 +4,9 @@
 		<meta charset="utf-8"/>
 		<title>Milestone-1</title>
 	</head>
-	<body></body>
+	<body>
+		<p><strong>Purpose:</strong> I want this website to be a professional development tool.</p>
+		<p><strong>Audience:</strong> This website will be designed for use by potential employers.</p>
+		<p><strong>Goal:</strong> To acquire full-time employment.</p>
+	</body>
 </html>
