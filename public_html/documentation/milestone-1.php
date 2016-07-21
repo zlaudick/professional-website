01865a6b570971a5fb64a52b3354b865fcd4d5db
Zac Laudick <zlaudick@nmsu.edu>
2016-07-20 23:16:49 -0600

    added Persona and Use Case to milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,5 +8,15 @@
 		<p><strong>Purpose:</strong> I want this website to be a professional development tool.</p>
 		<p><strong>Audience:</strong> This website will be designed for use by potential employers.</p>
 		<p><strong>Goal:</strong> To acquire full-time employment.</p>
+		<h2><strong>Persona</strong></h2>
+		<p><strong>Name:</strong> Jon Snow</p>
+		<p><strong>Age:</strong> 25</p>
+		<p><strong>Profession:</strong> Jon Snow is CEO of The Night's Watch, a web development company specializing in web application security solutions. Jon's business is growing rapidly and he expects a large increase in workload starting this winter. He needs to hire a new full-time, full stack web developer to help with the extra workload.</p>
+		<p><strong>Technology:</strong> Jon is a Mac power user. He has an iPhone 6s with unlimited data and a Macbook Pro laptop reinforced with a new valyrian steel casing. At work he connects his Macbook Pro to dual 30inch monitor displays.</p>
+		<p><strong>Attitudes and Behaviors:</strong> Jon is overworked and understaffed. Due to increased business demands he only has about 5 minutes to look over a potential candidate's resume and portfolio site before making a decision to contact them for an initial interview.</p>
+		<p><strong>Frustrations and Needs:</strong> Jon is under a lot of pressure to hire a new web developer. The job pays well and he receives around 25 applicants per week. Jon needs to find applicants that have web development skills who also believe in Jon's philosophy of providing web application security to everyone.</p>
+		<p><strong>Goal:</strong> Jon's primary goal is to find potential candidates for his first round of interviews. With his busy work schedule he can only spend 5 minutes per candidate looking through their resume and portfolio site. Jon needs to quickly make it through the hiring process because the workload at The Night's Watch is increasing and winter is coming.</p>
+		<h2><strong>Use Case</strong></h2>
+		<p>It's 7pm on a Friday and Jon Snow has received a cover letter and resume from myself and 20 other potential candidates. Jon will spend around 5 minutes looking through each candidate's resume and portfolio site before making a decision to add them to the list of first round interviews. Jon is quickly looking through the portfolio sites for the highest quality applicants because his assistant has just informed him of a meeting with his uncle Benjen, who has just returned from a long vacation.</p>
 	</body>
 </html>
