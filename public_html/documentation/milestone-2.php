f54bb354028a8029d73fd1dfe4b1e9e4bea7287b
Zac Laudick <zlaudick@nmsu.edu>
2016-08-05 10:05:27 -0600

    added content to sitemap

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,6 +6,12 @@
 	</head>
 	<body>
 		<h1>Sitemap</h1>
+		<p>This site will be a single page layout. It will contain the following sections:</p>
+		<ul>
+			<li>About Me</li>
+			<li>Portfolio</li>
+			<li>Contact Form</li>
+		</ul>
 		<h1>Content Strategy</h1>
 		<h1>Wireframes</h1>
 	</body>
